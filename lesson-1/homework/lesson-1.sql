a028ed553faea371a32d8cf49c724d5e420418af
Timaxrus <timaxrus@gmail.com>
2025-04-03 22:15:18 +0500

    for review

---
+++ lesson-1/homework/lesson-1.sql
@@ -0,0 +1,40 @@
+/* 
+
+===================
+I. EASY
+===================
+
+1. DEFINE THE FOLLOWING TERMS: DATA, DATABASE, RELATIONAL DATABASE, TABLE
+
+- DATA: Raw, unprocessed facts and figures. It can be any collection of characters, 
+numbers, symbols, images, or sounds. It's the basic building blocks of information.
+
+- DATABASE: A database is an organized collection of data, designed for efficient storage, retrieval, and management. 
+It allows users to store and access large amounts of information in a structured way.
+
+- RELATIONAL DATABASE: A relational database is a type of database that organizes data into one or more tables with defined 
+relationships between them. These relationships are established using keys, allowing for efficient data retrieval and manipulation.
+
+- TABLE: In a relational database, a table is a structure that organizes data into rows and columns. Each row represents a 
+record, and each column represents a specific attribute of that record. 
+
+2. LIST FIVE KEY FEATURES OF SQL SERVER.
+
+- High Performance and Scalability
+- Advanced Security Features
+- Business Intelligence and Analytics
+- High Availability and Disaster Recovery
+- Advanced Query Processing and Optimization
+
+3. WHAT ARE THE DIFFERENT AUTHENTIFICATION MODES AVAILABLE WHEN CONNECTING TO SQL SERVER (Give at least 2)?
+
+- Windows Authentification Mode: 
+- SQL Server Authentication Mode
+
+===================
+II. MEDIUM
+===================
+
+*/
+
+CREATE DATABASE SchoolDB
