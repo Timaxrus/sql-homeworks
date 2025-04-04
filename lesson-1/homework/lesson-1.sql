e69e608e76de9683ab514dd7949ec6fe3f696498
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-04-04 16:45:43 +0500

    Update lesson-1.sql

--- lesson-1/homework/lesson-1.sql
+++ lesson-1/homework/lesson-1.sql
@@ -1,8 +1,8 @@
-/* 
+-- ===================
+-- II. MEDIUM
+-- ===================
 
-===================
-I. EASY
-===================
+/* 
 
 1. DEFINE THE FOLLOWING TERMS: DATA, DATABASE, RELATIONAL DATABASE, TABLE
 
@@ -31,10 +31,66 @@ record, and each column represents a specific attribute of that record.
 - Windows Authentification Mode: 
 - SQL Server Authentication Mode
 
-===================
-II. MEDIUM
-===================
-
 */
 
-CREATE DATABASE SchoolDB
+-- ===================
+-- II. MEDIUM
+-- ===================
+
+-- 4. Create a new database in SSMS named SchoolDB.
+
+CREATE DATABASE SchoolDB;
+
+-- 5. Write and execute a query to create a table called Students with columns: StudentID (INT, PRIMARY KEY), Name (VARCHAR(50)), Age (INT).
+
+CREATE TABLE Students (
+  StudentID INT PRIMARY KEY,
+  Name VARCHAR(50),
+  Age INT );
+
+-- 6. Describe the differences between SQL Server, SSMS, and SQL.
+
+/*
+    SQL Server is the database engine that manages the data.
+    SSMS is the tool that's used to interact with SQL Server, manage databases, and execute queries.
+    SQL is the language you use within SSMS (or other tools) to query and manipulate the data stored in SQL Server.
+*/
+
+
+-- ===================
+-- III. HARD
+-- ===================
+
+-- 7. Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
+
+/*
+
+--> DQL (Data Query Language) 
+
+Commands: SELECT
+DQL is used to query or retrieve data from the database.
+
+Example: SELECT * FROM Students;    -- Retrieves all the columns from Students table
+
+
+--> DML (Data Manipulation Language) 
+
+Commands: INSERT, UPDATE, DELETE
+DML (INSERT) adds new record to a table, (UPDATE) modifies existing records in a table, (DELETE) removes records from a table.
+
+Example: INSERT INTO Students (StudentID, Name, AGE) VALUES (1, 'Timur', 34)
+         UPDATE Students SET AGE = 35 WHERE StudentID = 1;
+         DELETE FROM Students WHERE StudentID = 2;
+
+
+--> DDL (Data Definition Language) 
+
+Commands: CREATE, ALTER, DROP, TRUNCATE, RENAME
+DDL (CREATE) creates new database object, (ALTER) modifies the structure of an existing table, (DROP) deletes a database object, (TRUNCATE) removes all rows from a table without changing the table structure.
+
+Example: CREATE TABLE Employees (EmployeeID INT PRIMARY KEY, Name VARCHAR(50), Salary DECIMAL(9, 2);
+         ALTER TABLE Employees ADD Email VARCHAR(100);
+         DROP VIEW Employee
+    DCL (Data Control Language) 
+    TCL (Transaction Control Language) 
+     
