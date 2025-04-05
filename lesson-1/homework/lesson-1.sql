15154c2f03c294d1bb2693a72b50f07fee1a4d67
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-04-05 12:30:10 +0500

    Update lesson-1.sql

--- lesson-1/homework/lesson-1.sql
+++ lesson-1/homework/lesson-1.sql
@@ -61,36 +61,82 @@ CREATE TABLE Students (
 -- III. HARD
 -- ===================
 
--- 7. Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
-
 /*
 
+-- 7. Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
+
+==============================================================================================
 --> DQL (Data Query Language) 
 
 Commands: SELECT
 DQL is used to query or retrieve data from the database.
 
-Example: SELECT * FROM Students;    -- Retrieves all the columns from Students table
+Example: */
 
+SELECT * FROM Students;    -- Retrieves all the columns from Students table
 
+/*
+==============================================================================================
 --> DML (Data Manipulation Language) 
 
 Commands: INSERT, UPDATE, DELETE
 DML (INSERT) adds new record to a table, (UPDATE) modifies existing records in a table, (DELETE) removes records from a table.
 
-Example: INSERT INTO Students (StudentID, Name, AGE) VALUES (1, 'Timur', 34)
-         UPDATE Students SET AGE = 35 WHERE StudentID = 1;
-         DELETE FROM Students WHERE StudentID = 2;
+Example: */
 
+INSERT INTO Students (StudentID, Name, AGE) VALUES (1, 'Timur', 34)
+UPDATE Students SET AGE = 35 WHERE StudentID = 1;
+DELETE FROM Students WHERE StudentID = 2;
 
+/*
+==============================================================================================
 --> DDL (Data Definition Language) 
 
 Commands: CREATE, ALTER, DROP, TRUNCATE, RENAME
 DDL (CREATE) creates new database object, (ALTER) modifies the structure of an existing table, (DROP) deletes a database object, (TRUNCATE) removes all rows from a table without changing the table structure.
 
-Example: CREATE TABLE Employees (EmployeeID INT PRIMARY KEY, Name VARCHAR(50), Salary DECIMAL(9, 2);
-         ALTER TABLE Employees ADD Email VARCHAR(100);
-         DROP VIEW Employee
-    DCL (Data Control Language) 
-    TCL (Transaction Control Language) 
+Example: */
+
+CREATE TABLE Employees (EmployeeID INT PRIMARY KEY, Name VARCHAR(50), Salary DECIMAL(9, 2);
+ALTER TABLE Employees ADD Email VARCHAR(100);
+DROP VIEW Demographics;
+TRUNCATE TABLE Employees;
+
+/*
+==============================================================================================
+--> DCL (Data Control Language) 
+
+Commands: GRANT, REVOKE
+DCL (GRANT) gives specific privileges or permissions to a user or role, (REVOKE) removes previously granted privileges or permissions from a user or role.
+
+Example: */
+
+GRANT SELECT ON Employees TO Bill;
+REVOKE SELECT ON Employees FROM Bill;
+
+/*
+==============================================================================================
+--> TCL (Transaction Control Language) 
+
+Commands: COMMIT, ROLLBACK, SAVEPOINT, SET TRANSACTION.
+TCL (COMMIT) saves all changes made during the current transaction to the database permanently, (ROLLBACK) undoes all changes made during the current transaction, 
+restoring the database to its state before the transaction began, (SAVEPOINT) creates a point within a transaction to which you can later roll back without affecting 
+the entire transaction, (SET TRANSACTION) configures the properties of a transaction, such as its isolation level.
+
+Example: */
+
+SAVEPOINT BeforeUpdate;
+  
+UPDATE Employees
+SET Salary = Salary + 100
+WHERE EmployeeID = 2;
+
+-- If everything is fine, commit the changes
+COMMIT;
+
+-- If there's an issue, rollback the changes
+ROLLBACK TO BeforeUpdate;
+
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
      
