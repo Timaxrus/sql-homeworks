3a44926c6eedc64b83c5e398cc3d769f1c75f376
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-04-06 13:50:38 +0500

    Update lesson-1.sql

--- lesson-1/homework/lesson-1.sql
+++ lesson-1/homework/lesson-1.sql
@@ -2,9 +2,8 @@
 -- I. EASY
 -- ===================
 
-/* 
 
-1. DEFINE THE FOLLOWING TERMS: DATA, DATABASE, RELATIONAL DATABASE, TABLE
+-- 1. DEFINE THE FOLLOWING TERMS: DATA, DATABASE, RELATIONAL DATABASE, TABLE
 
 - DATA: Raw, unprocessed facts and figures. It can be any collection of characters, 
 numbers, symbols, images, or sounds. It's the basic building blocks of information.
@@ -18,7 +17,7 @@ relationships between them. These relationships are established using keys, allo
 - TABLE: In a relational database, a table is a structure that organizes data into rows and columns. Each row represents a 
 record, and each column represents a specific attribute of that record. 
 
-2. LIST FIVE KEY FEATURES OF SQL SERVER.
+-- 2. LIST FIVE KEY FEATURES OF SQL SERVER.
 
 - High Performance and Scalability
 - Advanced Security Features
@@ -26,12 +25,11 @@ record, and each column represents a specific attribute of that record.
 - High Availability and Disaster Recovery
 - Advanced Query Processing and Optimization
 
-3. WHAT ARE THE DIFFERENT AUTHENTIFICATION MODES AVAILABLE WHEN CONNECTING TO SQL SERVER (Give at least 2)?
+-- 3. WHAT ARE THE DIFFERENT AUTHENTIFICATION MODES AVAILABLE WHEN CONNECTING TO SQL SERVER (Give at least 2)?
 
 - Windows Authentification Mode: 
 - SQL Server Authentication Mode
 
-*/
 
 -- ===================
 -- II. MEDIUM
@@ -50,80 +48,78 @@ CREATE TABLE Students (
 
 -- 6. Describe the differences between SQL Server, SSMS, and SQL.
 
-/*
     SQL Server is the database engine that manages the data.
     SSMS is the tool that's used to interact with SQL Server, manage databases, and execute queries.
     SQL is the language you use within SSMS (or other tools) to query and manipulate the data stored in SQL Server.
-*/
+
 
 
 -- ===================
 -- III. HARD
 -- ===================
 
-/*
 
 -- 7. Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
 
 ==============================================================================================
---> DQL (Data Query Language) 
+DQL (Data Query Language) 
 
 Commands: SELECT
 DQL is used to query or retrieve data from the database.
 
-Example: */
+Example:
 
 SELECT * FROM Students;    -- Retrieves all the columns from Students table
 
-/*
-==============================================================================================
---> DML (Data Manipulation Language) 
+
+-- ==============================================================================================
+DML (Data Manipulation Language) 
 
 Commands: INSERT, UPDATE, DELETE
 DML (INSERT) adds new record to a table, (UPDATE) modifies existing records in a table, (DELETE) removes records from a table.
 
-Example: */
+Example:
 
 INSERT INTO Students (StudentID, Name, AGE) VALUES (1, 'Timur', 34)
 UPDATE Students SET AGE = 35 WHERE StudentID = 1;
 DELETE FROM Students WHERE StudentID = 2;
 
-/*
-==============================================================================================
---> DDL (Data Definition Language) 
+
+-- ==============================================================================================
+DDL (Data Definition Language) 
 
 Commands: CREATE, ALTER, DROP, TRUNCATE, RENAME
 DDL (CREATE) creates new database object, (ALTER) modifies the structure of an existing table, (DROP) deletes a database object, (TRUNCATE) removes all rows from a table without changing the table structure.
 
-Example: */
+Example:
 
 CREATE TABLE Employees (EmployeeID INT PRIMARY KEY, Name VARCHAR(50), Salary DECIMAL(9, 2);
 ALTER TABLE Employees ADD Email VARCHAR(100);
 DROP VIEW Demographics;
 TRUNCATE TABLE Employees;
 
-/*
-==============================================================================================
---> DCL (Data Control Language) 
+
+-- ==============================================================================================
+DCL (Data Control Language) 
 
 Commands: GRANT, REVOKE
 DCL (GRANT) gives specific privileges or permissions to a user or role, (REVOKE) removes previously granted privileges or permissions from a user or role.
 
-Example: */
+Example: 
 
 GRANT SELECT ON Employees TO Bill;
 REVOKE SELECT ON Employees FROM Bill;
 
-/*
-==============================================================================================
---> TCL (Transaction Control Language) 
+
+-- ==============================================================================================
+TCL (Transaction Control Language) 
 
 Commands: COMMIT, ROLLBACK, SAVEPOINT, SET TRANSACTION.
 TCL (COMMIT) saves all changes made during the current transaction to the database permanently, (ROLLBACK) undoes all changes made during the current transaction, 
 restoring the database to its state before the transaction began, (SAVEPOINT) creates a point within a transaction to which you can later roll back without affecting 
 the entire transaction, (SET TRANSACTION) configures the properties of a transaction, such as its isolation level.
 
-Example: */
+Example: 
 
 SAVEPOINT BeforeUpdate;
   
@@ -149,7 +145,7 @@ INSERT INTO Students VALUES
 
 -- 9. Create a backup of your SchoolDB database and restore it. (write its steps to submit)
 
-/*
+
 
 ===> CREATING A BACKUP OF SchoolDB:
 
@@ -165,7 +161,7 @@ Method 1: Using SQL Server Management Studio (SSMS) GUI
 5. Click OK to execute the backup
 6. Verify backup completed successfully in the Messages tab
 
-Method 2: Using T-SQL Command */
+Method 2: Using T-SQL Command 
 
 BACKUP DATABASE SchoolDB 
 TO DISK = 'C:\Backups\SchoolDB_Backup.bak'
@@ -175,7 +171,6 @@ WITH
    NAME = 'SchoolDB-Full Backup';
 GO
 
-/*
 
 ====> RESTORING SchoolDB
 
@@ -193,7 +188,7 @@ Method 1: Using SSMS GUI
 5. Click OK to execute the restore
 6. Verify restore completed successfully
 
-Method 2: Using T-SQL Command */
+Method 2: Using T-SQL Command 
 
 USE [master]
 GO
