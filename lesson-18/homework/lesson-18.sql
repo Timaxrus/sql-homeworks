2724f3be91567d084d95857ba526145884ca7755
Timaxrus <timaxrus@gmail.com>
2025-05-06 23:34:36 +0500

    update

--- lesson-18/homework/lesson-18.sql
+++ lesson-18/homework/lesson-18.sql
@@ -86,3 +86,133 @@ RETURN
 	);
 
 SELECT * FROM fn_GetSalesByCategory('Clothing')
+
+-- 5.
+
+CREATE OR ALTER function dbo.fn_IsPrime (@Number INT)
+Returns VARCHAR(50)
+AS
+BEGIN
+	DECLARE @Result VARCHAR(50);
+	DECLARE @i INT = 2;
+	DECLARE @IsPrime BIT = 1;
+	
+	IF @Number <= 1
+	BEGIN
+		SET @Result = 'No';
+	END
+	ELSE
+	BEGIN
+
+		WHILE @i <= SQRT(@Number)
+		BEGIN
+			IF @Number % @i = 0
+			BEGIN
+				SET @IsPrime = 0;
+				BREAK;
+			END
+			SET @i = @i + 1;
+		END
+		IF @IsPrime = 1
+			SET @Result = 'Yes';
+		ELSE
+			SET @Result = 'No';
+	END
+	RETURN @Result;
+
+END;
+
+SELECT dbo.fn_IsPrime(9) AS IsPrime;
+
+-- 6.
+
+Create function dbo.fn_GetNumbersBetween (@Start INT, @End INT)
+RETURNS TABLE
+AS
+RETURN 
+	(
+		WITH SeqNum AS (
+			SELECT
+				@Start AS Num
+			UNION ALL
+			SELECT
+				Num + 1 AS Num
+			FROM SeqNum
+			WHERE Num + 1 <= @End
+		)
+			SELECT Num FROM SeqNum
+	);
+
+SELECT * FROM dbo.fn_GetNumbersBetween(2, 15);
+
+-- 7.
+
+CREATE OR ALTER FUNCTION dbo.udf_getHighestSalary(@rnk INT)
+RETURNS TABLE
+AS
+RETURN
+(
+WITH RnkSal AS(
+SELECT
+	id,
+	salary,
+	ROW_NUMBER() OVER(ORDER BY salary DESC) AS RNK
+FROM Employee
+)
+SELECT DISTINCT
+	salary AS HighestNSalary
+FROM RnkSal
+WHERE RNK = @rnk
+UNION ALL
+SELECT
+	NULL AS HighestNSalary
+WHERE NOT EXISTS (SELECT 1 FROM RnkSal WHERE RNK = @rnk)
+);
+
+SELECT * FROM dbo.udf_getHighestSalary(5);
+
+
+-- 8.
+
+WITH TotalCount AS (
+SELECT
+	requester_id AS id,
+	COUNT(*) Total
+FROM Friends
+GROUP BY requester_id
+UNION
+SELECT
+	accepter_id AS id,
+	COUNT(*)
+FROM Friends
+GROUP BY accepter_id)
+
+SELECT TOP 1
+	id,
+	SUM(Total) AS num
+FROM TotalCount
+GROUP BY id
+ORDER BY SUM(Total) DESC;
+
+-- 9.
+
+CREATE VIEW vw_CustomerOrderSummary AS
+SELECT DISTINCT
+	c.customer_id,
+	c.name,
+	COUNT(o.order_id) OVER(PARTITION BY c.customer_id) AS total_orders,
+	SUM(o.amount) OVER(PARTITION BY c.customer_id) AS total_amount,
+	LAST_VALUE(o.order_date) OVER(PARTITION BY c.customer_id ORDER BY o.order_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_order_date
+FROM Customers AS c
+JOIN Orders AS o
+	ON c.customer_id = o.customer_id;
+
+SELECT * FROM vw_CustomerOrderSummary;
+
+-- 10.
+
+SELECT
+    RowNumber,
+    MAX(Workflow) OVER (ORDER BY RowNumber ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Workflow
+FROM TestTable;
+
