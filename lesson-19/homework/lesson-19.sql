109b4cede96ce9b398a58dc7d8c2669351b91587
Timur Abdukarimov <67995666+Timaxrus@users.noreply.github.com>
2025-05-07 16:04:29 +0500

    Update lesson-19.sql

--- lesson-19/homework/lesson-19.sql
+++ lesson-19/homework/lesson-19.sql
@@ -29,6 +29,24 @@ EXEC dbo.udp_EmployeeBonus;
 
 -- 2.
 
+CREATE OR ALTER PROCEDURE dbo.udp_UpdatedSalary @Dep VARCHAR(50), @bonus FLOAT
+AS
+BEGIN
+
+	SELECT
+    	CONCAT(FirstName, ' ', LastName) AS FullName,
+        Department,
+        Salary AS OldSalary,
+        (Salary * @bonus / 100) + Salary AS NewSalary
+    FROM Employees
+    WHERE Department = @Dep;
+
+END;
+
+EXEC dbo.udp_UpdatedSalary @Dep ='HR', @bonus = 10;
+
+-- 3.
+
 MERGE INTO Products_Current AS TARGET
 USING Products_New AS Source
 	ON TARGET.ProductID = SOURCE.ProductID
@@ -43,3 +61,69 @@ WHEN NOT MATCHED BY SOURCE THEN
 
 
 SELECT * FROM Products_Current;
+
+-- 4.
+
+SELECT
+	id,
+    CASE WHEN p_id IS NULL THEN 'Root'
+    	WHEN id NOT IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf'
+        ELSE 'Inner'
+    END AS type
+FROM Tree;
+
+-- 5.
+
+WITH Confirm AS (
+SELECT
+	s.user_id,
+    c.action,
+    COUNT(action) AS byType
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+	on s.user_id = c.user_id
+Group by s.user_id, c.action
+HAVING c.action = 'confirmed'
+)
+, OverAll AS (
+SELECT
+	s.user_id,
+  	COUNT(c.action) AS Total
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+	on s.user_id = c.user_id
+Group by s.user_id
+)
+SELECT
+	o.user_id,
+    COALESCE(CAST(c.byType AS FLOAT) / NULLIF(CAST(o.Total AS FLOAT), 0), 0) AS rate
+FROM Confirm AS c
+RIGHT JOIN OverAll AS o
+	on c.user_id = o.user_id;
+
+-- 6. 
+
+SELECT
+	name,
+    salary
+FROM employees
+WHERE salary = (SELECT MIN(salary) FROM Employees);
+
+-- 7.
+
+CREATE PROCEDURE dbo.udp_GetProductSalesSummary @ProdID INT
+AS
+BEGIN
+  SELECT DISTINCT
+      p.ProductName,
+      SUM(s.Quantity)  OVER(PARTITION BY p.ProductName) AS TotalQuantity,
+      SUM(s.Quantity) OVER(PARTITION BY p.ProductName) * p.Price AS TotalSalesAmount,
+      FIRST_VALUE(s.SaleDate) OVER(PARTITION BY p.ProductName ORDER BY s.SaleDate) AS FirstSaleDate,
+      LAST_VALUE(s.SaleDate) OVER(PARTITION BY p.ProductName ORDER BY s.SaleDate ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS FirstSaleDate
+  FROM Products AS p
+  LEFT JOIN Sales AS s
+      ON p.ProductID = s.ProductID
+  WHERE p.ProductID = @ProdID;
+END;
+
+EXEC dbo.udp_GetProductSalesSummary @ProdID = 1;
